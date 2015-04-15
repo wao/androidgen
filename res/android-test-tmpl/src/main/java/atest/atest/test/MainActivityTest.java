ed7ba1b9a31d11f8ae24d3d094656b1c450269e0
Yang Chen <mike.c.yang@gmail.com>
2015-04-15 11:22:01 +0800

    Add robotium support

--- res/android-test-tmpl/src/main/java/atest/atest/test/MainActivityTest.java
+++ res/android-test-tmpl/src/main/java/atest/atest/test/MainActivityTest.java
@@ -1,9 +1,27 @@
 package <%= package_name %>.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
 import <%= package_name %>.*;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity_> {
+
+    private Solo solo;
+
+    @Override
+    public void setUp() throws Exception {
+        //setUp() is run before a test case is started. 
+        //This is where the solo object is created.
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        //tearDown() is run after a test case has finished. 
+        //finishOpenedActivities() will finish all the activities that have been opened during the test execution.
+        solo.finishOpenedActivities();
+    }
+
 
     public MainActivityTest() {
         super(MainActivity_.class); 
